35118b1fda2835b2c6768c6b3573e8e4c0dfa1d4
Davide Polonio <poloniodavide@gmail.com>
2017-11-02 09:52:21 +0100

    Minor shared prefs fix

--- app/src/main/java/com/spacerace/augugrumi/spacerace/utility/SharedPreferencesManager.java
+++ app/src/main/java/com/spacerace/augugrumi/spacerace/utility/SharedPreferencesManager.java
@@ -4,7 +4,7 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.util.Log;
 
-import com.spacerace.augugrumi.spacerace.MyApplication;
+import com.spacerace.augugrumi.spacerace.SpaceRace;
 
 
 /**
@@ -15,13 +15,13 @@ import com.spacerace.augugrumi.spacerace.MyApplication;
 
 public class SharedPreferencesManager {
 
-    private static final String SHARED_PREFERENCES_FILE = "sharedPreferencesFile";
+    private static final String SHARED_PREFERENCES_FILE = "com.spacerace.augugrumi";
     private static final String FIRST_RUN = "first_application_run";
 
     public static boolean getFirstApplicationRun() {
         Log.i("SHARED_PREFERENCE_MAN", "app run");
 
-        Context context = MyApplication.getAppContext();
+        Context context = SpaceRace.getAppContext();
         SharedPreferences sharedPref = context.getSharedPreferences(
                 SHARED_PREFERENCES_FILE, Context.MODE_PRIVATE);
 
@@ -30,7 +30,7 @@ public class SharedPreferencesManager {
 
     public static void setFirstApplicationRun(boolean run) {
         Log.i("SHARED_PREFERENCE_MAN", "app run " + run);
-        Context context = MyApplication.getAppContext();
+        Context context = SpaceRace.getAppContext();
         SharedPreferences sharedPref = context.getSharedPreferences(
                 SHARED_PREFERENCES_FILE, Context.MODE_PRIVATE);
 
