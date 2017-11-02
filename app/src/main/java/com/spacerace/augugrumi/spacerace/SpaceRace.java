00f4f39359ebae43f4c1ffc17d55104df508da83
Davide Polonio <poloniodavide@gmail.com>
2017-11-02 10:15:25 +0100

    New things

--- app/src/main/java/com/spacerace/augugrumi/spacerace/SpaceRace.java
+++ app/src/main/java/com/spacerace/augugrumi/spacerace/SpaceRace.java
@@ -1,8 +1,16 @@
 package com.spacerace.augugrumi.spacerace;
 
+import android.app.AlertDialog;
 import android.app.Application;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.support.annotation.NonNull;
 import android.util.Log;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.drive.Drive;
+import com.google.android.gms.games.Games;
 
 /**
  * @author Marco Zanella
@@ -13,15 +21,47 @@ import android.util.Log;
 public class SpaceRace extends Application {
 
     private static Context instance;
+    private static GoogleApiClient gAPIClient;
 
     @Override
     public void onCreate() {
         super.onCreate();
 
         instance = this;
+
+        gAPIClient = new GoogleApiClient.Builder(instance)
+                .addOnConnectionFailedListener(new GoogleApiClient.OnConnectionFailedListener() {
+                    @Override
+                    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+                        Log.d("INTRO", "Something went wrong with Google Play Games connection...");
+
+                        new AlertDialog.Builder(instance)
+                                .setTitle(getResources().getText(R.string.warningSlider3Title))
+                                .setMessage(getResources().getText(R.string.warningSlider3SubTitle))
+                                .setIcon(android.R.drawable.ic_dialog_alert)
+                                .setNegativeButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                                        // Nothing?
+                                    }
+                                })
+                                .show();
+                    }
+                })
+                .addApi(Games.API).addScope(Games.SCOPE_GAMES)
+                .addApi(Drive.API).addScope(Drive.SCOPE_APPFOLDER)
+                .build();
+
     }
 
     public static Context getAppContext() {
         return instance;
     }
+
+    public static GoogleApiClient getgAPIClient() {
+        return gAPIClient;
+    }
+
 }
--- app/src/main/java/com/spacerace/augugrumi/spacerace/intro/slides/PlayGamesSlideFragment.java
+++ app/src/main/java/com/spacerace/augugrumi/spacerace/intro/slides/PlayGamesSlideFragment.java
@@ -17,6 +17,7 @@ import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.drive.Drive;
 import com.google.android.gms.games.Games;
 import com.spacerace.augugrumi.spacerace.R;
+import com.spacerace.augugrumi.spacerace.SpaceRace;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -24,14 +25,12 @@ import butterknife.ButterKnife;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class PlayGamesSlideFragment extends Fragment
-        implements GoogleApiClient.OnConnectionFailedListener {
-
+public class PlayGamesSlideFragment extends Fragment {
     @BindView(R.id.playButton)
     SignInButton gPlaySignInButton;
 
 
-    private GoogleApiClient gAPIClient;
+    private final GoogleApiClient gAPIClient = SpaceRace.getgAPIClient();
 
     public PlayGamesSlideFragment() {
 
@@ -52,11 +51,6 @@ public class PlayGamesSlideFragment extends Fragment
 
         ButterKnife.bind(this, v);
 
-        gAPIClient = new GoogleApiClient.Builder(getContext())
-                .addOnConnectionFailedListener(this)
-                .addApi(Games.API).addScope(Games.SCOPE_GAMES)
-                .addApi(Drive.API).addScope(Drive.SCOPE_APPFOLDER)
-                .build();
 
         gPlaySignInButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -71,24 +65,5 @@ public class PlayGamesSlideFragment extends Fragment
 
         // Inflate the layout for this fragment
         return v;
-    }
-
-    @Override
-    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-
-        Log.d("INTRO", "Something went wrong with Google Play Games connection...");
-
-        new AlertDialog.Builder(getContext())
-                .setTitle(getResources().getText(R.string.warningSlider3Title))
-                .setMessage(getResources().getText(R.string.warningSlider3SubTitle))
-                .setIcon(android.R.drawable.ic_dialog_alert)
-                .setNegativeButton(android.R.string.ok, new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-
-                        // Nothing?
-                    }
-                })
-                .show();
     }
 }
