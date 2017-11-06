4173fb19e457f4b7637813c7dfb98b92930064d6
Davide Polonio <poloniodavide@gmail.com>
2017-11-06 10:22:37 +0100

    Fix double play services login

--- app/src/main/java/com/augugrumi/spacerace/MainActivity.java
+++ app/src/main/java/com/augugrumi/spacerace/MainActivity.java
@@ -45,6 +45,7 @@ public class MainActivity extends AppCompatActivity implements
     final static int RC_WAITING_ROOM = 10002;
 
     private ProgressDialog mProgressDialog;
+    private boolean alreadySignIn = false;
 
     @BindView(R.id.main_activity)
     View myView;
@@ -58,32 +59,42 @@ public class MainActivity extends AppCompatActivity implements
 
         ButterKnife.bind(this);
 
-        setUpGoogleApi();
+        if (alreadySignIn){
+
+            setUpGoogleApi();
+            alreadySignIn = false;
+        }
+
     }
 
     @Override
     public void onStart() {
         super.onStart();
 
-        OptionalPendingResult<GoogleSignInResult> opr = Auth.GoogleSignInApi.silentSignIn(mGoogleApiClient);
-        if (opr.isDone()) {
-            // If the user's cached credentials are valid, the OptionalPendingResult will be "done"
-            // and the GoogleSignInResult will be available instantly.
-            Log.d("INTRO", "Got cached sign-in");
-            GoogleSignInResult result = opr.get();
-            handleSignInResult(result);
-        } else {
-            // If the user has not previously signed in on this device or the sign-in has expired,
-            // this asynchronous branch will attempt to sign in the user silently.  Cross-device
-            // single sign-on will occur in this branch.
-            showProgressDialog();
-            opr.setResultCallback(new ResultCallback<GoogleSignInResult>() {
-                @Override
-                public void onResult(GoogleSignInResult googleSignInResult) {
-                    hideProgressDialog();
-                    handleSignInResult(googleSignInResult);
-                }
-            });
+        if (alreadySignIn) {
+
+            OptionalPendingResult<GoogleSignInResult> opr = Auth.GoogleSignInApi.silentSignIn(mGoogleApiClient);
+            if (opr.isDone()) {
+                // If the user's cached credentials are valid, the OptionalPendingResult will be "done"
+                // and the GoogleSignInResult will be available instantly.
+                Log.d("INTRO", "Got cached sign-in");
+                GoogleSignInResult result = opr.get();
+                handleSignInResult(result);
+            } else {
+                // If the user has not previously signed in on this device or the sign-in has expired,
+                // this asynchronous branch will attempt to sign in the user silently.  Cross-device
+                // single sign-on will occur in this branch.
+                showProgressDialog();
+                opr.setResultCallback(new ResultCallback<GoogleSignInResult>() {
+                    @Override
+                    public void onResult(GoogleSignInResult googleSignInResult) {
+                        hideProgressDialog();
+                        handleSignInResult(googleSignInResult);
+                    }
+                });
+            }
+
+            alreadySignIn = false;
         }
     }
 
@@ -101,8 +112,6 @@ public class MainActivity extends AppCompatActivity implements
 
             SharedPreferencesManager.setFirstApplicationRun(true);
         }
-        /*
-        } else {*/
     }
 
     @Override
