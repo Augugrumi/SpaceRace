d5ea63a324185e16e2118320a0068bdaf74ba7b8
Davide Polonio <poloniodavide@gmail.com>
2017-11-28 18:47:44 +0100

    Try to make emoji rain working with onTouch() event

--- app/src/main/java/com/augugrumi/spacerace/EndMatchActivity.java
+++ app/src/main/java/com/augugrumi/spacerace/EndMatchActivity.java
@@ -3,6 +3,9 @@ package com.augugrumi.spacerace;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
+import android.util.Log;
+import android.view.View;
+import android.view.MotionEvent;
 
 import com.luolc.emojirain.EmojiRainLayout;
 
@@ -11,10 +14,12 @@ import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class EndMatchActivity extends AppCompatActivity {
-
+    
     @BindView(R.id.end_match_activity)
     EmojiRainLayout endMatch;
 
+    private boolean isAlreadyDropped = false;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,6 +32,29 @@ public class EndMatchActivity extends AppCompatActivity {
         endMatch.addEmoji(R.drawable.endmatch_wind_chime);
         endMatch.addEmoji(R.drawable.endmatch_balloon);
         endMatch.addEmoji(R.drawable.endmatch_trophy);
+
+	endMatch.setDuration(5000);
+	endMatch.setDropFrequency(250);
+
+	endMatch.stopDropping();
+
+	endMatch.setOnTouchListener(new View.OnTouchListener() {
+
+		@Override
+		public boolean onTouch(View view, MotionEvent event) {
+
+		    if (!isAlreadyDropped) {
+
+			Log.d("DROPPING", "Emoji rain :D");
+			
+			isAlreadyDropped = true;
+			endMatch.startDropping();
+		    }		   
+		    
+		    return true;
+		}
+	    });
+
     }
 
     @OnClick(R.id.rain_test)
